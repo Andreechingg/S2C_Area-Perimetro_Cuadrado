0cbd0d13fa49b2a340d2454d8a155333f242c54c
Andreechingg <a20150155@juan23.edu.pe>
2024-11-13 12:39:35 -0500

    Initial commit

---
+++ src/cuadrado.js
@@ -0,0 +1,16 @@
+// Titulo
+document.write("<h1>Ejercicio 03</h1>");
+document.write("<h3>Área y perímetro de un cuadrado</h3>");
+//Solicitar informacion
+var lado = parseInt(prompt("Ingrese el lado del cuadrado"));
+var area = lado * lado;
+var perimetro = lado * 4;
+//Mostrar informacion
+document.write("El lado ingresado fue:" + lado);
+document.write("<br>");
+document.write("El área del cuadrado fue:" + area);
+document.write("<br>");
+document.write("El perímetro del cuadrado es " + perimetro);
+document.write("<br>");
+document.write("<br>");
+document.write("<h5>Esta aplicación fue creada por Andreé Ching</h5>");
